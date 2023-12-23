795b30761c11e3b5234c041b48baca7be9d881d4
ChampX5 <oshal.baranwal@gmail.com>
2023-12-23 22:19:13 +0530

    forgot to commit the config file for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,14 @@
-export default {};
+export default {
+    theme: {
+        extend: {
+            colors: {
+                'border-grey': '#393939',
+                'light-grey': '#2e2c35',
+                'light-grey-hover': '#393642',
+                grey: '#1c1c1c',
+                secondary: '#ffffff49',
+                primary: '#ffffff'
+            }
+        }
+    }
+};
